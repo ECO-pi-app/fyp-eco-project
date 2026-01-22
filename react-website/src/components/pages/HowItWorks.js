0b2630d0fc887188301943542a1c7c2550a31626
Lohithroshan45 <lohithroshan.23@ichat.sp.edu.sg>
2026-01-22 11:47:47 +0800

    a

--- react-website/src/components/pages/HowItWorks.js
+++ react-website/src/components/pages/HowItWorks.js
@@ -1,29 +1,18 @@
 import React from "react";
-import "../../App.css";
-import "./About_us.css";
+import "./HowItWorks.css";
 
 export default function HowItWorks() {
   return (
-    <div
-      className="about about--howitworks"
-      style={{ "--banner-img": "url(/images/img-1.jpg)" }}
-    >
-      <div className="about__overlay">
-        <div className="about__container">
-          <header className="about__header">
-            <h1 className="about__title">App tutorial</h1>
-            <p className="about__subtitle">
-              Watch the walkthrough to learn how to use the app.
-            </p>
-          </header>
+    <div className="howitworks">
+      <div className="howitworks__card">
+        <h1 className="howitworks__title">Sign in tutorial</h1>
 
-          <section className="about__videoWrap">
-            <video className="about__video" controls muted>
-              <source src="/videos/log-in.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </section>
-        </div>
+        <video
+          className="howitworks__video"
+          src="/videos/signin.mp4"
+          controls
+          playsInline
+        />
       </div>
     </div>
   );
